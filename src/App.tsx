15ad2dd29b621410dc26d464178b115e430987fc
Gabriel Henrique <gabrielhenrique16041999@gmail.com>
2025-05-23 16:53:30 -0300

    pagina inicial encaminhada, faltando o input

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Container } from './components/Container'
-import { Heading } from './components/Heading'
 import { Logo } from './components/Logo'
 import { Menu } from './components/Menu'
 
 
 import './styles/theme.css'
 import './styles/global.css'
+import { CountDown } from './components/Countdown'
+import { DefaultInput } from './components/DefaultInput'
 
 
 
@@ -13,8 +14,26 @@ function App() {
 
     return (
         <>
-            <Container><Heading><Logo/></Heading></Container>
-            <Container><Heading><Menu /></Heading></Container>
+            <Container><Logo/></Container>
+            <Container><Menu /></Container>
+            <Container><CountDown/></Container>
+            <Container>
+                <form className='form' action="">
+                    <div className="formRow">
+                        <DefaultInput id='meuInput' type='number' />
+                    </div>
+                    <div className="formRow">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+                    <div className="formRow">
+                        <p>Ciclos</p>
+                        <p>0 0 0 0 0</p>
+                    </div>
+                    <div className="formRow">
+                        <button>Enviar</button>
+                    </div>
+                </form>
+            </Container>
         </>
         
     ) 
